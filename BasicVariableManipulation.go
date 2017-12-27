a31586f7b1fd7135cf968db6c8aa704a652e7d81
Rocia <rocia.fernandes@gmail.com>
2017-12-27 19:33:15 +0530

    Basic variable manipulation program

---
+++ BasicVariableManipulation.go
@@ -0,0 +1,27 @@
+package main
+import "fmt"
+func main(){
+
+//strings
+var a string = "initial" //explicit declaration
+var b = "initial2" //simple declaration
+fmt.Println(a)
+fmt.Println(b)
+
+//Integers
+var c,d int = 12,15
+var e int 		//auto set to 0
+fmt.Println(c,d)
+fmt.Println(e)
+
+//Boolean
+var f = false
+fmt.Println(f)
+fmt.Println(!f)
+
+//Auto type declaration
+g := "short"
+h := 2
+fmt.Println(g)
+fmt.Println(h)
+}
