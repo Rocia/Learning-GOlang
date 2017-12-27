ad91b268a678a43e90c8610910e08c85c3a8f6f8
Rocia <rocia.fernandes@gmail.com>
2017-12-27 19:13:50 +0530

    Basic data type manipulation program

---
+++ BasicValueManipulation.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main(){
+// strings added together with "+" are concatenated whereas those added with , are simply printed together with a space in between.
+fmt.Println("GO"+"lang", "tutorial")
+//Integer and Float types
+fmt.Println("2+2 = ",2+2)
+fmt.Println("2*2 = ",2*2)
+fmt.Println("12/5 = ", 12/5)
+fmt.Println("12.0/5.0 =", 12.0/5.0)
+}
