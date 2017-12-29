be577e92d1c81994c05acb6d0916e48d985ec21b
Rocia <rocia.fernandes@gmail.com>
2017-12-29 11:09:41 +0530

    Anonymous and Recursive Functions

---
+++ TypesOfFunctions.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+//There are two main kinds of functions we will explore here
+//Anonymous and Recursive
+
+func RescursiveFactorial(a int) int {						//Functions called by any number of trailing arguments
+	if a == 0 {
+		return 1
+	} else {
+		return a * RescursiveFactorial(a-1)
+	}
+}
+
+
+
+func AnonymousFibonacci() func() int {
+	a := 0
+	b := 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+
+}
+
+func iterations() int {
+	return 5
+}
+
+func main(){
+	Anonymous := AnonymousFibonacci()
+	iterations := iterations()
+	fmt.Println("Recursive Factorial", RescursiveFactorial(iterations))
+	fmt.Println("Anonymous Fibonaccci")
+	for iterations != 0{
+		fmt.Println(Anonymous())
+		iterations --
+	}
+
+}
+
+/*
+Recursive Factorial 120
+Anonymous Fibonaccci
+1
+1
+2
+3
+5
+ */
