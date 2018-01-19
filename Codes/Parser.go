df0f3d7206952f6bfa9b3654873e7836beae89cd
Rocia <rocia.fernandes@gmail.com>
2018-01-19 16:42:48 +0530

    Number & URL parsing in GO

--- Codes/Parser.go
+++ Codes/Parser.go
@@ -3,7 +3,8 @@ package main
 import (
 	"strconv"		//For number parsing
 	"fmt"
-	"go/constant"
+	"net"
+	"net/url"
 )
 func number_parser(){
 	f, _ := strconv.ParseFloat("1.234", 64)			//Parse float 64 says how many bits of precision to parse
@@ -25,8 +26,64 @@ func number_parser(){
 	fmt.Println(e)
 }
 
+func url_parser(){
+	s := "https://user:pass@host.com:5432/path?shirt_color=red#f"
+
+	u, err := url.Parse(s)		// Parse the URL and ensure there are no errors.
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(u.Scheme)		//Access the scheme
+
+	fmt.Println(u.User)				//call user
+	fmt.Println(u.User.Username())	//user contains username and password
+	p, _ := u.User.Password()
+	fmt.Println(p)
+
+	fmt.Println(u.Host)				//If the host is present it will contain bith the hostname and the port
+	host, port, _ := net.SplitHostPort(u.Host)
+	fmt.Println(host)
+	fmt.Println(port)
+
+	fmt.Println(u.Path) 			//Extract path and the fragment after the #
+	fmt.Println(u.Fragment)
+
+	fmt.Println(u.RawQuery)			//To get all the parameters in a key value pair format use the raw query command
+	m, _ := url.ParseQuery(u.RawQuery)	//You can also parse these into a map
+	fmt.Println(m)
+	fmt.Println(m["shirt_color"][0]) // As the values are available in the form of slices you can access it using its index position
+}
 
 func main() {
+
 	number_parser()
 
+	fmt.Println("________________________________________________")
+
+	url_parser()
+
 }
+
+
+/*
+1.234
+123
+456
+789
+135
+strconv.ParseInt: parsing "What": invalid syntax
+________________________________________________
+https
+user:pass
+user
+pass
+host.com:5432
+host.com
+5432
+/path
+f
+shirt_color=red
+map[shirt_color:[red]]
+red
+ */
